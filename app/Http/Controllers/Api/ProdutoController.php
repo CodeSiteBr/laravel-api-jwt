3cb77b6013b63737c52e15a7eef51490e9a7ca61
Marcelo Camargo <marcelo.camargo@tvfsoftware.com>
2019-02-21 19:48:26 -0300

    Adicionando a API de Produtos

---
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Produto;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index()
+    {
+        return Produto::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Produto::create($request->all());
+    }
+
+    public function show($id)
+    {
+        $produto = Produto::find($id);
+        if (!$produto) {
+            return response()->json([
+                'error' => 'Produto não encontrado'
+            ], 404);
+        }
+
+        return $produto;
+    }
+
+    public function edit(Produto $produto)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        $produto = Produto::find($id);
+        if (!$produto) {
+            return response()->json([
+                'error' => 'Produto não encontrado'
+            ], 404);
+        }
+
+        $produto->update($request->all());
+        return $produto;
+    }
+
+    public function destroy($id)
+    {
+        $produto = Produto::find($id);
+        if (!$produto) {
+            return response()->json([
+                'error' => 'Produto não encontrado'
+            ], 404);
+        }
+
+        $produto->delete();
+        return $produto;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,37 +1,47 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::prefix('v1')->group(function () {
+    Route::post('login', 'Api\AuthController@login')->name('api.v1.login');
+    Route::post('refresh', 'Api\AuthController@refresh')->name('api.v1.refresh');
+
+    Route::group(['middleware' => ['auth:api','jwt.refresh'], 'namespace' => 'Api', 'as' => 'api.v1.'], function () {
+        Route::post('logout', 'AuthController@logout')->name('logout');
+
+        Route::resource('usuarios', 'UserController', ['except' => ['create', 'edit']]);
+        Route::resource('produtos', 'ProdutoController', ['except' => ['create', 'edit']]);
+    });
+});
+
+Route::resource('produtos', 'Api\ProdutoController', ['as' => 'api', 'except' => ['create', 'edit']]);
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+    Route::resource('produtos', 'ProdutoController');
+
+    Essa declaração de rota resource cria várias rotas para manipular
+    uma variedade de ações RESTful no recurso de foto.
+    Da mesma forma, o controlador gerado já terá métodos para cada uma
+    dessas ações, incluindo notas informando quais URIs e verbos eles
+    manipulam.
+
+    +-----------+--------------------------+------------------+----------------------------+
+    | Method    | URI                      | Name             | Action                     |
+    +-----------+--------------------------+------------------+----------------------------+
+    | GET|HEAD  | /produtos                | produtos.index   | ProdutoController@index    |
+    | GET|HEAD  | /produtos/{produto}      | produtos.show    | ProdutoController@show     |
+    | GET|HEAD  | /produtos/create         | produtos.create  | ProdutoController@create   |
+    | POST      | /produtos                | produtos.store   | ProdutoController@store    |
+    | GET|HEAD  | /produtos/{produto}/edit | produtos.edit    | ProdutoController@edit     |
+    | PUT|PATCH | /produtos/{produto}      | produtos.update  | ProdutoController@update   |
+    | DELETE    | /produtos/{produto}      | produtos.destroy | ProdutoController@destroy  |
+    +-----------+--------------------------+------------------+----------------------------+
+
+    // Rotas manuais que o tipo resource faz
+
+    Route::get('/produtos', 'ProdutoController@index')->name('produtos.index');
+    Route::get('/produtos/{produto}', 'ProdutoController@show')->name('produtos.show');
+    Route::get('/produtos/create', 'ProdutoController@create')->name('produtos.create');
+    Route::post('/produtos', 'ProdutoController@store')->name('produtos.store');
+    Route::get('/produtos/{produto}/edit', 'ProdutoController@edit')->name('produtos.edit');
+    Route::put('/produtos/{produto}', 'ProdutoController@update')->name('produtos.update');
+    Route::delete('/produtos/{produto}', 'ProdutoController@destroy')->name('produtos.destroy');
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-Route::name('api.login')->post('login', 'Api\AuthController@login');
-Route::post('refresh', 'Api\AuthController@refresh');
-
-
-//Req 1 - token XXX - refresh token - YYY
-//req2 - token XXX
-//req3 - token XXX
-
-Route::group(['middleware' => ['auth:api','jwt.refresh']], function(){
-    Route::get('users', function(){
-        return \App\User::all();
-    });
-    Route::post('logout', 'Api\AuthController@logout');
-    //Route::resource('clients', 'ClientController', ['except' => ['create', 'edit']]);
-});
-
-//Auth::guard('api')->user()
