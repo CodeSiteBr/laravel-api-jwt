dd1b03c2eabbe6dfaef91fe34f799f3c271e4465
Marcelo Camargo <marcelo.camargo@tvfsoftware.com>
2019-02-22 09:49:01 -0300

    Adicionando validação para o produto

--- app/Http/Controllers/Api/ProdutoController.php
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Produto;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Api\ProdutoRequest as Request;
 
 class ProdutoController extends Controller
 {
